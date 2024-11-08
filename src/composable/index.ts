6df12f8c3fd2a06280089b48ceede1268d057426
Meer Sagor <meersagor.contact@gmail.com>
2024-11-09 02:03:43 +0600

    feat: added new utility for generateImageUrlToBase64

--- src/composable/index.ts
+++ src/composable/index.ts
@@ -1,4 +1,5 @@
 import { logGenerateFormData, generateFormData } from './formData';
 import {loadCustomFontFace} from './customFontFace'
+import {generateImageUrlToBase64} from './generateImageUrlToBase64'
 
-export { logGenerateFormData, generateFormData, loadCustomFontFace };
+export { logGenerateFormData, generateFormData, loadCustomFontFace, generateImageUrlToBase64 };
